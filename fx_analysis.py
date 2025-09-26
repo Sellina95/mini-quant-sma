922c5200a80e1f96ce00fcc81170785a20d8901f
Sellina95 <seyeon8163@gmail.com>
2025-09-26 12:06:16 +0900

    Update fx_analysis.py

--- fx_analysis.py
+++ fx_analysis.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import numpy as np
+import matplotlib.pyplot as plt
 
 # 1. 원/달러 환율 데이터 가져오기 (KRW=X)
 df = yf.download("KRW=X", period="1mo", interval="1d")
@@ -12,5 +13,9 @@ avg_rate = df['Close'].mean()
 returns = df['Close'].pct_change().dropna()
 volatility = returns.std() * np.sqrt(252)
 
+df['Close'].plot(title="KRW/USD 최근 1개월 환율")
+
+
 print("최근 1개월 평균 환율:", round(avg_rate, 2), "KRW/USD")
 print("최근 1개월 연율화 변동성:", round(volatility*100, 2), "%")
+plt.show()
